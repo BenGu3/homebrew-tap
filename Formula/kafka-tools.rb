31e58aa02e364e07fa23782e600071db876595a1
Ben Guthrie <benjee.012@gmail.com>
2021-12-29 10:28:23 -0700

    kafka-tools 1.0.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/kafka-tools.rb
+++ Formula/kafka-tools.rb
@@ -3,10 +3,10 @@
 class KafkaTools < Formula
   desc "kafka-tools CLI"
   homepage "https://github.com/bengu3/kafka-tools"
-  url "url"
-  sha256 "sha"
+  url "https://github.com/bengu3/kafka-tools/releases/download/v1.0.0/kafka-tools.tar.gz"
+  sha256 "38ad1d2314b5f9777ccd4a47e2cfff1aefa2c8232d5f892216a83ff72a41d7c8"
   license "MIT"
-  version "0.0.0"
+  version "1.0.0"
 
   def install
     bin.install "kafka-tools"
